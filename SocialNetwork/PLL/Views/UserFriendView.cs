8eaf691c7abef3a0c36f6ea57f8a476bc98d429a
Dmitry Perepelov <dperepelov@logicstars.ru>
2022-10-13 19:40:07 +0300

    Add project files.

---
+++ SocialNetwork/PLL/Views/UserFriendView.cs
@@ -0,0 +1,23 @@
+﻿using SocialNetwork.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class UserFriendView
+    {
+        public void Show(IEnumerable<User> friends)
+        {
+            Console.WriteLine("Мои друзья");
+
+            friends.ToList().ForEach(friend =>
+            {
+                Console.WriteLine($"E-mail: {friend.Email}. Имя: {friend.FirstName}. Фамилия: {friend.LastName}");
+            });
+
+            }
+
+        }
+}
+
